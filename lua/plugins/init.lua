45a174b914512e1ac03e4fdf01c8f01f99a99524
Nikita Aw1nger <aw1nger@yandex.ru>
2024-10-31 09:47:45 +0300

    fix autocomplate from mac

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,11 +1,10 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre",
     opts = require "configs.conform",
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -13,13 +12,83 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-tree/nvim-tree.lua",
+    lazy = false,
+    config = function()
+      require("nvim-tree").setup {
+        git = {
+          enable = true,
+          ignore = false,
+          timeout = 400,
+        },
+        renderer = {
+          root_folder_label = false,
+        },
+      }
+    end,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+
+  {
+    "stevearc/dressing.nvim",
+    lazy = false,
+    opts = {},
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "go",
+      },
+    },
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+        "typescript-language-server",
+        "eslint-lsp",
+        "prettier",
+        "css-lsp",
+        "html-lsp",
+        "lua-language-server",
+        "stylua",
+      },
+    },
+  },
+
+  {
+    "mfussenegger/nvim-lint",
+    event = "VeryLazy",
+  },
+
+  {
+    "windwp/nvim-ts-autotag",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+
+  {
+    "folke/trouble.nvim",
+    lazy = false,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+
+  {
+    "Exafunction/codeium.vim",
+    lazy = false,
+  },
 }
